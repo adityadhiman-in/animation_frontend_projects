d16957ad3636c196d323ff5fcf381a339a1bcf96
Aditya Dhiman <info@adityadhiman.in>
2024-10-05 20:15:28 +0530

    scroll trigger II project basic css with basic animations

--- Scroll Trigger II/script.js
+++ Scroll Trigger II/script.js
@@ -1,5 +1,8 @@
 const hero = document.querySelector(".hero");
 const image = document.querySelector(".hero >.heroImg > img");
+const about = document.querySelector(".about");
+const aboutImage = document.querySelector(".about > .aboutImg > img");
+const allAnchors = document.querySelectorAll("a");
 
 hero.addEventListener("mousemove", (event) => {
   gsap.to(image, {
@@ -9,3 +12,23 @@ hero.addEventListener("mousemove", (event) => {
     ease: "power3.out",
   });
 });
+
+about.addEventListener("mousemove", (event) => {
+  gsap.to(aboutImage, {
+    x: 1- event.x * 0.05,
+    y: 1 - event.y * 0.05,
+    duration: 0.3,
+    ease: "power3.out",
+  });
+});
+
+// allAnchors.forEach((anchor) => {
+//   anchor.addEventListener("mousemove", (event) => {
+//     gsap.to(anchor,{
+//       scale: 1.1,
+//       duration: 0.3,
+//       ease: "power3.out",
+//       scrub: true
+//     })
+//   })
+// })
